af618f6cebc5f1b774c6c84c1f3b282323c5cc5e
Musa Jallow <musa.j.jallow@student.jyu.fi>
2020-07-11 15:09:28 +0300

    tuotantoversio lisätty

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ morgan.token('daTa', function getResponse (res) {
 app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :daTa"))
+app.use(express.static('build'))
 
 let persons = [
       {
